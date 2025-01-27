f10c0211a15878517da276dcf214b51f33cc59ca
DhruvGupta1075 <96821313+DhruvGupta1075@users.noreply.github.com>
2025-01-27 10:56:55 +0530

    Solution to Grade Based on Marks

---
+++ Grade Based on Marks@@6788dd63490637cfc44e1b96/code.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    int a;
+    scanf("%d",&a);
+    switch(a){
+        case 90{
+            printf("A");
+            break
+        }
+    }
+    return 0;
+}
