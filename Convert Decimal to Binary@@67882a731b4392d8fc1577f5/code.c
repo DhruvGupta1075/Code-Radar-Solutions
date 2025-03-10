629ce256fdc9d7e991c77b93d71be2b3911d6f9b
Dhruv Gupta <96821313+DhruvGupta1075@users.noreply.github.com>
2025-03-10 18:09:28 +0530

    Solution to Convert Decimal to Binary

--- Convert Decimal to Binary@@67882a731b4392d8fc1577f5/code.c
+++ Convert Decimal to Binary@@67882a731b4392d8fc1577f5/code.c
@@ -1,10 +1,21 @@
 #include <stdio.h>
+
+int MyPow(int a,int b){
+      if(b<0)      
+        return 1 / MyPow (a,-b)
+      else if(b==0)
+        return 1;
+      else if(b==1)
+        return a;
+      else
+        return a*MyPow(a,b-1)
+    }
 int main(){
     int d,ans,i=0;
     scanf("%d",&d);
     while (d!=0){
         int bit=d&1;
-        ans=(bit*pow(10,i))+ans;
+        ans=(bit*MyPow(10,i))+ans;
         i++;
         d=d>>1;
     }
