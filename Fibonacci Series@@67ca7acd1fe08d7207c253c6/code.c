9eb08039415a789a46d65f3ad619094acac67677
Dhruv Gupta <96821313+DhruvGupta1075@users.noreply.github.com>
2025-03-21 15:02:01 +0530

    Solution to Fibonacci Series

--- Fibonacci Series@@67ca7acd1fe08d7207c253c6/code.c
+++ Fibonacci Series@@67ca7acd1fe08d7207c253c6/code.c
@@ -1,8 +1,8 @@
 void fibonacciSeries(int n){
     int a=0,b=1,c=0;
-    while (n--){
+    for (int i=2;i<n;i++){
         c=a+b;
-        printf("%d",c);
+        printf("%d ",c);
         a=b;
         b=c;
     }
