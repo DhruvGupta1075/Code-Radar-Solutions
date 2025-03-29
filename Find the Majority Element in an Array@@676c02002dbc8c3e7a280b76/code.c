5b5d0f3b0b721ac9fbccce14e802d9c46b27d2bb
Dhruv Gupta <96821313+DhruvGupta1075@users.noreply.github.com>
2025-03-29 12:55:26 +0530

    Solution to Find the Majority Element in an Array

--- Find the Majority Element in an Array@@676c02002dbc8c3e7a280b76/code.c
+++ Find the Majority Element in an Array@@676c02002dbc8c3e7a280b76/code.c
@@ -5,12 +5,11 @@ int count(int arr[],int size){
     for (int i=0;i<size;i++){
         if (arr[i]==arr[i+1]){
             c++;
-            if (c>=size/2) {
-                return arr[i];
-            }
-            else return -1;
         }
-        
+        if (c>=size/2) {
+            return arr[i];
+        }
+        else return -1;
     }
     
 }
